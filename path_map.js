09d211edc7358fd0eb2aadb4f62820fb5520f950
Mel <Mel@makofintech.com>
2022-08-09 08:56:12 -0400

    finally finished the path mapper

--- path_map.js
+++ path_map.js
@@ -24,6 +24,12 @@ const example_schema = [{
                 }
             },
             {
+                "next": "jump_2",
+                "condition": {
+                    "field_2": "value_2"
+                }
+            },
+            {
                 "next": "page_4"
             }
         ]
@@ -43,13 +49,72 @@ const example_schema = [{
         "subtitle": "example_subtitle",
         "fields": [],
         "submit": true
+    },
+    {
+        "id": "jump_2",
+        "type": "jump",
+        "branches": [{
+                "next": "page_3",
+                "condition": {
+                    "field_3": "value_3"
+                }
+            },
+            {
+                "next": "page_6"
+            }
+        ]
+    },
+    {
+        "id": "page_6",
+        "type": "page",
+        "title": "example_title",
+        "subtitle": "example_subtitle",
+        "fields": [],
+        "next": "page_7"
+    },
+    {
+        "id": "page_7",
+        "type": "page",
+        "title": "example_title",
+        "subtitle": "example_subtitle",
+        "fields": [],
+        "next": "jump_3"
+    },
+    {
+        "id": "jump_3",
+        "type": "jump",
+        "branches": [{
+                "next": "page_8",
+                "condition": {
+                    "field_4": "value_4"
+                }
+            },
+            {
+                "next": "page_9"
+            }
+        ]
+    },
+    {
+        "id": "page_8",
+        "type": "page",
+        "title": "example_title",
+        "subtitle": "example_subtitle",
+        "fields": [],
+        "submit": true
+    },
+    {
+        "id": "page_9",
+        "type": "page",
+        "title": "example_title",
+        "subtitle": "example_subtitle",
+        "fields": [],
+        "submit": true
     }
 ];
 
 const RESERVED = Symbol('RESERVED');
 
-console.log('hello');
-console.log(RESERVED === RESERVED);
+// console.log(RESERVED === RESERVED);
 
 Object.prototype.first_value = function (self = this) {
     return Object.values(self)[0] ?? false;
@@ -64,32 +129,6 @@ Object.prototype.length = function () {
 };
 
 
-let path_map_v3 = [
-
-    [
-        ['page_1', {}],
-        ['page_2', {}],
-        ['jump_1', {}],
-        ['page_5', {
-            field_1: 'value_1'
-        }],
-        ['page_6', {
-            field_1: 'value_1'
-        }],
-        ['jump_2', {
-            field_1: 'value_1'
-        }],
-        ['page_7', {
-            "$and": [{
-                field_1: 'value_1'
-            }, {
-                field_2: 'value_a'
-            }]
-        }]
-    ]
-];
-
-
 /** Steps
  * 1) start from the first element
  * 2) for "page" types, find the next page, then the next and keep going. This is your array.
@@ -97,63 +136,74 @@ let path_map_v3 = [
  * 4) At any point, when you see "submit: true", do break;
  */
 
+var DONE = false;
+const result = [];
 
-let done = false;
-let result = [];
+console.log(map_constructor(example_schema));
 
-path_constructor(example_schema);
-
-console.log(path_constructor(example_schema));
-
-function path_constructor(schema) {
-    let temp = [];
-    let jumps_we_saw = [];
-    let jump_map_tracking = {};
+function map_constructor(schema) {
+    let RESULT = [
+        []
+    ];
     let submit, last_position, last_next;
 
     if (schema[0].submit === true) {
-        console.log('This schema has only a single page.');
+        console.warn('This schema has only a single page.');
         submit = true;
-        temp.push([schema[0].id, {}]);
+        RESULT[0].push([schema[0].id, {}]);
     } else if (schema[0].type === 'page') {
-        temp.push([schema[0].id, {}]);
+        RESULT[0].push([schema[0].id, {}]);
         last_position = schema[0].id;
         last_next = schema[0].next;
     } else if (schema[0].type === 'jump') {
-        jumps_we_saw.push(schema[0].id);
-        jump_map_tracking[schema[0].id] = {};
-        schema[0].branches.forEach(branch => {
-            jump_map_tracking[schema[0].id][branch] = branch
-        });
-
+        console.warn('starting with a jump condition is not supported (yet)');
+        process.exit();
     }
+
     let index = 0;
     while (!submit) {
-        let page = page_finder(schema, last_next); 
+        let page = page_finder(schema, last_next);
         last_position = page.id;
         if (page.submit === true) {
             submit = true;
-            temp.push([page.id, temp[index][1]]);
+            RESULT.forEach((el, ind) => {
+                RESULT[ind] = RESULT[ind].concat([
+                    [page.id, {}]
+                ]);
+            });
             break;
         } else if (page.type === 'page') {
             last_next = page.next;
-            temp.push([page.id, temp[index][1]]);
+            RESULT.forEach((el, ind) => {
+                RESULT[ind] = RESULT[ind].concat([
+                    [page.id, {}]
+                ]);
+
+            });
         } else if (page.type === 'jump') {
             let branches_from_here = jump_handler(schema, page);
-            console.log(branches_from_here);
-            // branches_from_here.forEach(path => {
-            //     temp.forEach((el, ind) => {
-            //         temp[ind] = temp[ind].concat(path)
-            //     });
-            // });
+            // console.log(branches_from_here);
+            let temp = [];
+            let stringified_result = JSON.stringify(RESULT);
+            for (let i = 1; i <= branches_from_here.length; i++) { //duplicating the paths
+                temp.push(JSON.parse(stringified_result));
+            };
+            temp = temp.flat(1);
+            branches_from_here.forEach((jump_path, jump_index) => {
+                temp[jump_index] = temp[jump_index].concat(jump_path);
+            });
+            RESULT = temp;
+            break; //not necessary??
         }
         index++;
-    }
-    return temp;
+    };
+    return RESULT;
 };
 
 function individual_path_constructor(schema, page_id, condition = {}) {
-    const temp = [[]];
+    let RESULT = [
+        []
+    ];
     let last_next = page_id;
     let submit;
     let index = 0;
@@ -161,61 +211,85 @@ function individual_path_constructor(schema, page_id, condition = {}) {
         let page = page_finder(schema, last_next);
         if (page.submit === true) {
             submit = true;
-            temp.forEach((el, ind) => {
-                temp[ind].push([page.id, condition_combiner(temp[0][index]?.[1] ?? {}, condition)])
+            RESULT.forEach((el, ind) => {
+                RESULT[ind] = RESULT[ind].concat([
+                    [page.id, condition_combiner(RESULT[0][index]?. [1] ?? {}, condition)]
+                ]);
             });
             break;
         } else if (page.type === 'page') {
             last_next = page.next;
-            temp.forEach((el, ind) => {
-                console.log(temp[0]);
-                temp[ind].push([page.id, condition_combiner(temp[0][index]?.[1] ?? {}, condition)])
+            RESULT.forEach((el, ind) => {
+                RESULT[ind] = RESULT[ind].concat([
+                    [page.id, condition_combiner(RESULT[0][index]?. [1] ?? {}, condition)]
+                ]);
+
             });
         } else if (page.type === 'jump') {
             let branches_from_here = jump_handler(schema, page, condition);
-            branches_from_here.forEach(path => {
-                temp.forEach((el, ind) => {
-                    temp[ind].push(temp[0].concat(path));
-                });
+            let temp = [];
+            let stringified_result = JSON.stringify(RESULT);
+            for (let i = 1; i <= branches_from_here.length; i++) { //duplicating the paths
+                temp.push(JSON.parse(stringified_result));
+            };
+            temp = temp.flat(1);
+            branches_from_here.forEach((jump_path, jump_index) => {
+                temp[jump_index] = temp[jump_index].concat(jump_path);
             });
+            RESULT = temp;
+            break;
         }
         index++;
     };
-    console.log(temp);
-    return temp;
+    return RESULT;
 };
-
-
 
 function jump_handler(schema, jump) {
-    let index = 0;
-    let submit;
-    let last_next = jump.id;
     let jump_result = {};
     jump.branches.forEach((branch, num) => {
-        console.log(branch);
-        jump_result[`branch_${num}`] = individual_path_constructor(schema, branch.next, branch.condition ?? {});
+        if (num + 1 === jump.branches.length) { //means the last branch
+            if (branch.condition) {
+                console.warn('The last branch is not a default branch. It should be.');
+            };
+            let default_condition;
+            if (jump.branches.length === 2) {
+                default_condition = {
+                    $not: jump.branches[0].condition
+                }
+            } else if (jump.branches.length > 2) {
+                default_condition = {
+                    $and: jump.branches.slice(0, -1).map(cond => {
+                        return {
+                            $not: cond.condition
+                        }
+                    })
+                };
+            } else {
+                console.warn(`Something is off. The branch length is: `, jump.branches.length);
+                default_condition = {};
+            };
+            jump_result[`branch_${num}`] = individual_path_constructor(schema, branch.next, default_condition);
+        } else {
+            jump_result[`branch_${num}`] = individual_path_constructor(schema, branch.next, branch.condition);
+        }
     });
-    console.log(jump_result);
-    return Object.values(jump_result);
+    let result = Object.values(jump_result).flat(1);
+    return result;
 };
-
-
-
 
 function condition_combiner(condition_1, condition_2) {
     if (Object.keys(condition_1).length === 0) return condition_2; //empty conditions
     if (Object.keys(condition_2).length === 0) return condition_1;
     let temp = {};
     if (condition_1.$and && condition_1.$and.some(cond => deepEqual(condition_2, cond))) { //if one contains the other entirely
-        console.log('condition_1 already contains condition_2');
+        console.warn('condition_1 already contains condition_2');
         return condition_1;
     } else if (condition_2.$and && condition_2.$and.some(cond => deepEqual(condition_1, cond))) {
-        console.log('condition_2 already contains condition_1');
+        console.warn('condition_2 already contains condition_1');
         return condition_2;
     };
     if (deepEqual(condition_1, condition_2)) { //identical
-        console.log('These two conditions are identical');
+        console.warn('These two conditions are identical');
         return condition_1;
     };
     if (condition_1.$and && condition_2.$and) { //merge the $and conditions
@@ -258,23 +332,6 @@ function deepEqual(x, y) {
     ) : (x === y);
 };
 
-
-
-/**
- * Feed an array and an object.
- * goes through the layers of the object looking at the keys as per the array.
- */
-function deep_digger(array_to_dig_by, obj_to_dig) {
-    let current = obj_to_dig;
-    array_to_dig_by.forEach(item => {
-        current = current[item]
-    });
-    return current;
-}
-
-
-
-
 function page_finder(schema, page_to_find) {
     let step_1 = schema.filter(page => page.id === page_to_find);
     if (step_1.length === 0) {
@@ -300,28 +357,7 @@ function array_equal(arr1, arr2, same_order = true) {
 };
 
 
-let array_1 = ["a", "b", "c", "", ""];
-let array_2 = ["a", "b", "", "c", "d"];
+// let array_1 = ["a", "b", "c", "d", ""];
+// let array_2 = ["a", "b", "c", "", "d"];
 
-console.log(array_equal(array_1, array_2, false));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function sidebar_customizer(path_map_v3) {
-
-};
+// console.log(deepEqual(array_1, array_2));
