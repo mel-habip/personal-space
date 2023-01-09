3630bcaf1c2081665de96fd624f8a69f488ccd60
Mel <Mel@makofintech.com>
2023-01-08 19:58:12 -0500

    further organized, moved functions out to separate files, added more `users` endpoints

--- Stock-Portfolio-Website/backend/jobs/authenticateToken.js
+++ Stock-Portfolio-Website/backend/jobs/authenticateToken.js
@@ -1,8 +1,15 @@
 import * as jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
+import env_dir from '../utils/env_dir.js';
+dotenv.config({
+    path: env_dir
+});
 
-
+/**
+ * @function authenticateToken - middleware that converts JWT to user and adds it to request data
+ * @returns {null} - adds `user` to request data
+ */
 export default function authenticateToken(req, res, next) { //this is middleware in /login
     const authHeader = req.headers.authorization;
     const token = authHeader?.split(' ')[1];
--- Stock-Portfolio-Website/stock_portfolio_db_script_1.sql
+++ Stock-Portfolio-Website/stock_portfolio_db_script_1.sql
@@ -5,15 +5,17 @@ CREATE TABLE Users (
   Username varchar(255),
   LastName varchar(255),
   FirstName varchar(255),
-  Permissions VARCHAR(100) DEFAULT 'client'
+  Permissions VARCHAR(100) DEFAULT 'client',
+  Active TINYINT DEFAULT '1',
+  CreatedOn DATETIME DEFAULT NOW(),
 );
 CREATE TABLE Positions (
 	PositionID int auto_increment primary key,
 	UserID Int NOT NULL,
-    Ticker VARCHAR(10) NOT NULL,
-    CreatedOn DATETIME DEFAULT NOW(),
-    AcquiredOn Date,
-    SoldOn Date,
-    Active TINYINT DEFAULT '1',
-    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+  Ticker VARCHAR(10) NOT NULL,
+  CreatedOn DATETIME DEFAULT NOW(),
+  AcquiredOn Date,
+  SoldOn Date,
+  Active TINYINT DEFAULT '1',
+  FOREIGN KEY (UserID) REFERENCES Users(UserID)
 )
